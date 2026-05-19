5e706db081528b3221dfce2857dfb0275d2264d4
BoveVincenzo <vincenzo.bove@studenti.unitn.it>
2026-05-19 12:23:22 +0200

    bug fix

--- src/pause_menu/systems.rs
+++ src/pause_menu/systems.rs
@@ -2,12 +2,7 @@ use bevy::prelude::*;
 use crate::app_state_manager::messages::PlayPressed;
 
 pub fn send_play_pressed(
-    mut reader: MessageReader<PlayPressed>,
     mut writer: MessageWriter<PlayPressed>)
 {
-    // if !reader.is_empty() {
-    //     reader.clear();
-    //     writer.write(PlayPressed);
-    // }
     writer.write(PlayPressed);
 }
