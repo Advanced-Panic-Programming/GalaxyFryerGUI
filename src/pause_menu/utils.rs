bdd1a185b9bf483fbefacdced4896f3ba9a517c2
BoveVincenzo <vincenzo.bove@studenti.unitn.it>
2026-05-26 12:23:15 +0200

    changed logo size

--- src/pause_menu/utils.rs
+++ src/pause_menu/utils.rs
@@ -10,7 +10,7 @@ pub const BUTTON_HEIGHT: f32 = 80.0;
 pub const FONT_SIZE: f32 = 42.0;
 
 pub const BUTTON_SPACING: f32 = 40.0;
-pub const LOGO_SCALE: f32 = 1.0;
+pub const LOGO_SCALE: f32 = 1.6;
 
 pub const LOGO_FRAME_SIZE: u32 = 400;
 pub const LOGO_FPS: f32 = 6.0;
